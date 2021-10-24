99a583fae62c01db9f95b68c37e64dd7a3affc9d
Fouladgar <fouladgar.dev@gmail.com>
2021-10-24 16:30:38 +0330

    fix notification fake

--- tests/OTPBrokerTest.php
+++ tests/OTPBrokerTest.php
@@ -57,7 +57,7 @@ class OTPBrokerTest extends TestCase
 
         Notification::assertSentTo(
             $user,
-            function (OTPNotification $notification, $channels) {
+            function ($notification, $channels) {
                 return $channels[0] == config('otp.channel');
             }
         );
