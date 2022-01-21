b06d6ac56ce6c3d2fbff7537ee45db5906d2ab4d
Fouladgar <fouladgar.dev@gmail.com>
2022-01-21 14:07:54 +0330

    compatible with php7.3

--- tests/database/factories/NotifiableUserFactory.php
+++ tests/database/factories/NotifiableUserFactory.php
@@ -5,6 +5,8 @@
 use Faker\Generator as Faker;
 use Fouladgar\OTP\Tests\Models\OTPNotifiableUser;
 
-$factory->define(OTPNotifiableUser::class, fn (Faker $faker) => [
-    'mobile' => '555555',
-]);
+$factory->define(OTPNotifiableUser::class, function (Faker $faker) {
+    return [
+        'mobile' => '555555',
+    ];
+});
