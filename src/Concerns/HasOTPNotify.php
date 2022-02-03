e07c75a663b53e964e449d7c50659ffd8c754806
mohammad-fouladgar <mohammad-fouladgar@users.noreply.github.com>
2022-02-03 10:46:28 +0000

    Fix styling

--- src/Concerns/HasOTPNotify.php
+++ src/Concerns/HasOTPNotify.php
@@ -32,7 +32,7 @@ trait HasOTPNotify
     {
         $mobileFiled = $this->getMobileField();
 
-        if (!in_array($mobileFiled, $this->fillable, true)) {
+        if (! in_array($mobileFiled, $this->fillable, true)) {
             $this->fillable = array_merge($this->fillable, [$mobileFiled]);
         }
     }
