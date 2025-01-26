39a54d875d8f5ce4969e1eeb377199a1beb5180e
Your Name <fateme.besharat766@gmail.com>
2025-01-26 17:21:31 +0330

    fix exception message handling

--- src/Exceptions/OTPException.php
+++ src/Exceptions/OTPException.php
@@ -8,16 +8,16 @@ class OTPException extends Exception
 {
     public static function whenOtpTokenIsInvalid(): static
     {
-        return new static('The token has been expired or invalid.');
+        return new static('token_has_been_expired_or_invalid');
     }
 
     public static function whenUserNotFoundByMobile(): static
     {
-        return new static('User not found by mobile.');
+        return new static('user_not_found_by_mobile');
     }
 
     public static function whenOtpAlreadySent(): static
     {
-        return new static('OTP has already been sent for this mobile.');
+        return new static('otp_has_already_been_sent_for_this_mobile');
     }
 }
