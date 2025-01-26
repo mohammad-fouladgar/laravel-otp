0d3c0a8bb0899258882e0de4d52df3681ca92726
Your Name <fateme.besharat766@gmail.com>
2025-01-26 17:21:31 +0330

    fix exception message handling

--- lang/en/otp.php
+++ lang/en/otp.php
@@ -13,4 +13,10 @@ return [
     'otp_token' => 'Your OTP Token is: :token.',
 
     'otp_subject' => 'OTP request',
+
+    'token_has_been_expired_or_invalid' => 'the token has been expired or invalid',
+
+    'user_not_found_by_mobile' => 'user not found by mobile',
+
+    'otp_has_already_been_sent_for_this_mobile' => 'OTP has already been sent for this mobile',
 ];
