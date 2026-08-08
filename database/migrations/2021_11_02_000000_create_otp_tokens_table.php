ac8949b3e8192fe1614b8e143ffa451c679bb4e4
fouladgar <fouladgar.dev@gmail.com>
2026-08-08 16:36:19 +0330

    fix migration

--- database/migrations/2021_11_02_000000_create_otp_tokens_table.php
+++ database/migrations/2021_11_02_000000_create_otp_tokens_table.php
@@ -8,45 +8,34 @@ class CreateOTPTokensTable extends Migration
 {
     private string $tokenTable;
 
-    private string $userTable;
-
-    private string $mobileColumn;
-
     public function __construct()
     {
-        $default            = config('otp.default_provider', 'users');
-        $this->userTable    = config('otp.user_providers.'.$default.'.table', 'users');
-        $this->mobileColumn = config('otp.mobile_column', 'mobile');
-        $this->tokenTable   = config('otp.token_table', 'otp_tokens');
+        $this->tokenTable = config('otp.token_table', 'otp_tokens');
     }
 
     public function up(): void
     {
-        if (!Schema::hasColumn($this->userTable, $this->mobileColumn)) {
-            Schema::table(
-                $this->userTable,
-                function (Blueprint $table): void {
-                    $table->string($this->mobileColumn);
-                }
-            );
-        }
-
         if (config('otp.token_storage') === 'cache') {
             return;
         }
 
-        Schema::create(
-            $this->tokenTable,
-            static function (Blueprint $table): void {
-                $table->increments('id');
-                $table->string('mobile')->index();
-                $table->string('token', 10)->index();
-                $table->timestamp('sent_at')->nullable();
-                $table->timestamp('expires_at')->nullable();
+        if (Schema::hasTable($this->tokenTable)) {
+            return;
+        }
 
-                $table->index(['mobile', 'token']);
-            }
-        );
+        Schema::create($this->tokenTable, static function (Blueprint $table): void {
+            $table->increments('id');
+            $table->string('recipient')->index();
+            $table->string('purpose');
+            $table->string('token', 10);
+            $table->timestamp('sent_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+
+            $table->index(
+                ['recipient', 'purpose', 'token'],
+                'otp_recipient_purpose_token_index',
+            );
+        });
     }
 
     public function down(): void
@@ -54,6 +43,7 @@ class CreateOTPTokensTable extends Migration
         if (config('otp.token_storage') === 'cache') {
             return;
         }
-        Schema::drop($this->tokenTable);
+
+        Schema::dropIfExists($this->tokenTable);
     }
 }
